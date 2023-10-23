e8d4cc821ca27ed86bb4ff1b79c7f082887c81db
Ruan Sampaio <jose.ruan@totvs.com.br>
2023-10-22 22:34:06 -0300

    finalizando desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+  import java.util.Locale; // Importe a classe Locale
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    //System.out.println("Hello, World!");
+    //todo: conhecern e importa a classe scanner
+    //exibir as msgs para usuariuo
+    //obter pela classe scanner os vvalores digitados no terminal
+    //em seguida mostrar as mensagens
+    
+    Scanner sc = new Scanner(System.in);
+
+    // Defina a localização para usar o ponto como separador decimal
+    sc.useLocale(Locale.US);
+    
+    System.out.println("Informe seu nome");
+    String nome = sc.nextLine();
+
+    System.out.println("Por favor, digite o número da Agência !");
+    String agencia = sc.nextLine();
+
+    System.out.println("Por favor, digite o número da conta !");
+    int conta = sc.nextInt();
+
+    System.out.println("Por favor, digite o saldo da conta !");
+    float saldo = sc.nextFloat();
+
+
+    //mensagem final
+    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+    ", conta" + conta + " e seu saldo R$ " + saldo +" já está disponível para saque");
+
+
+    sc.close();
+    
     }
 }
